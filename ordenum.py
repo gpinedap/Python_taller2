a027c908d21fbb9c52c4592058078ed8a8206970
gilbert pineda <gpinedap@unal.edu.co>
2018-09-01 06:16:25 -0500

    agrego primer programa que ordena n numeros

---
+++ ordenum.py
@@ -0,0 +1,17 @@
+import sys
+n=int(raw_input('N: '))
+a=[]
+for i in range(n):
+	
+	c=float(raw_input('Entre numero: '))
+	a.append(c)
+	print (a)
+for j in range (1,n-1):
+	for k in range (j+1, n):
+		
+                if a[k] < a[j]: 
+                    aux=a[j]
+                    a[j]=a[k]
+                    a[k]=aux
+print (a)
+			 
